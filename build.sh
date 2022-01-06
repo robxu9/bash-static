66636259290de368448f7c29ae1bf0e1449e4434
Robert Xu <me@robxu9.com>
2022-01-05 20:59:56 -0500

    an attempt to use zig cc as a compiler instead

--- build.sh
+++ build.sh
@@ -30,7 +30,14 @@ set -o pipefail
 # load version info
 . version.sh
 
-platform=$(uname -s)
+target="$1"
+
+if [ -z "$target" ]; then
+  echo "Missing target tuple to build (./build.sh <target-tuple>)" >&2
+  exit 1
+fi
+
+echo "./build.sh called with target $1"
 
 if [ -d build ]; then
   echo "= removing previous build directory"
@@ -44,9 +51,7 @@ pushd build
 echo "= preparing gpg"
 export GNUPGHOME="$(mktemp -d)"
 # public key for bash
-gpg --batch --keyserver hkp://keyserver.ubuntu.com --recv-keys 7C0135FB088AAF6C66C650B9BB5869F064EA74AB
-# public key for musl
-gpg --batch --keyserver hkp://keyserver.ubuntu.com --recv-keys 836489290BB6B70F99FFDA0556BCDB593020450F
+gpg --batch --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7C0135FB088AAF6C66C650B9BB5869F064EA74AB
 
 # download tarballs
 echo "= downloading bash"
@@ -69,49 +74,53 @@ for lvl in $(seq $bash_patch_level); do
     popd
 done
 
-if [ "$platform" = "Linux" ]; then
-  if [ "$(cat /etc/os-release | grep ID= | head -n1)" = "ID=alpine" ]; then
-    echo "= skipping installation of musl because this is alpine linux (and it is already installed)"
-  else
-    echo "= downloading musl"
-    curl -LO https://musl.libc.org/releases/musl-${musl_version}.tar.gz
-    curl -LO https://musl.libc.org/releases/musl-${musl_version}.tar.gz.asc
-    gpg --batch --verify musl-${musl_version}.tar.gz.asc musl-${musl_version}.tar.gz
-
-    echo "= extracting musl"
-    tar -xf musl-${musl_version}.tar.gz
-
-    echo "= building musl"
-    working_dir=$(pwd)
-
-    install_dir=${working_dir}/musl-install
-
-    pushd musl-${musl_version}
-    ./configure --prefix=${install_dir}
-    make install
-    popd # musl-${musl-version}
-
-    echo "= setting CC to musl-gcc"
-    export CC=${working_dir}/musl-install/bin/musl-gcc
-  fi
-  export CFLAGS="-static"
+# set minimum version of macOS to 10.13
+export MACOSX_DEPLOYMENT_TARGET="10.13"
+# bash uses CC_FOR_BUILD
+if [[ "$target" != *"macos"* ]]; then
+  export CC_FOR_BUILD="zig cc -target x86_64-linux-musl -static -Wl,-Bstatic"
 else
-  echo "= WARNING: your platform does not support static binaries."
-  echo "= (This is mainly due to non-static libc availability.)"
-  if [ "$platform" = "Darwin" ]; then
-    # set minimum version of macOS to 10.13
-    export MACOSX_DEPLOYMENT_TARGET="10.13"
-    # https://www.gnu.org/software/bash/manual/html_node/Compilers-and-Options.html
-    export CC="gcc -std=c89 -Wno-implicit-function-declaration -Wno-return-type"
-  fi
+  export CC_FOR_BUILD="zig cc"
 fi
+
+# https://www.gnu.org/software/bash/manual/html_node/Compilers-and-Options.html
+export CC="zig cc -target $target -static -Wl,-Bstatic -std=c89 -Wno-implicit-function-declaration -Wno-return-type"
+export AR="zig ar"
+export RANLIB="zig ranlib"
+export LD="zig build-lib"
 
 echo "= building bash"
 
+# get the host for cross-compiling
+wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
+wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+
+chmod +x config.guess config.sub
+export CC_BUILD="$(./config.guess)"
+export CC_TARGET="$(./config.sub $target)"
+
 pushd bash-${bash_version}
-CFLAGS="$CFLAGS -Os" ./configure --without-bash-malloc
+sed -i s/\$lo/@OBJEXT@/g lib/intl/Makefile.in
+autoconf -f
+cat > config.cache << "EOF"
+ac_cv_func_mmap_fixed_mapped=yes
+ac_cv_func_strcoll_works=yes
+ac_cv_func_working_mktime=yes
+bash_cv_func_sigsetjmp=present
+bash_cv_getcwd_malloc=yes
+bash_cv_job_control_missing=present
+bash_cv_printf_a_format=yes
+bash_cv_sys_named_pipes=present
+bash_cv_ulimit_maxfds=yes
+bash_cv_under_sys_siglist=yes
+bash_cv_unusable_rtsigs=no
+gt_cv_int_divbyzero_sigfpe=yes
+EOF
+./configure --build="$CC_BUILD" --host="$CC_TARGET" --without-bash-malloc --enable-static-link --cache-file=config.cache
 make
-make tests
+if [[ "$target" == *"linux"* ]] || [[ "$target" == *"macos"* ]]; then
+  make tests
+fi
 popd # bash-${bash_version}
 
 popd # build
